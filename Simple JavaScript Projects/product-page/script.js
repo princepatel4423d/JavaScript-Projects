c37af7f35dfee09bc9d0e895e7bc5253d894b75d
Prince Patel <patelprince2598@gmail.com>
2024-12-19 16:38:08 +0530

    Add files via upload

---
+++ Simple JavaScript Projects/product-page/script.js
@@ -0,0 +1,24 @@
+let productImg = document.getElementById("productImg");
+let btn = document.getElementsByClassName("btn");
+
+btn[0].onclick = function() {
+    productImg.src = "./assets/image1.png";
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[1].onclick = function() {
+    productImg.src = "./assets/image2.png";
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[2].onclick = function() {
+    productImg.src = "./assets/image3.png";
+    for (bt of btn) {
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
